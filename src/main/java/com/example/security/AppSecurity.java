b4c55dd723223c7eb36a0c9d7ee6ce0f03fb9d15
Ranjeet Rathour <rathourm99@gmail.com>
2024-08-19 13:28:45 +0530

    add https

--- src/main/java/com/example/security/AppSecurity.java
+++ src/main/java/com/example/security/AppSecurity.java
@@ -1,13 +1,9 @@
 package com.example.security;
 
-import com.example.authentication.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,25 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class AppSecurity {
-//
-//    private final CustomAuthenticationProvider customAuthenticationProvider;
-//
-//    @Autowired
-//    public AppSecurity(CustomAuthenticationProvider customAuthenticationProvider) {
-//        this.customAuthenticationProvider = customAuthenticationProvider;
-//    }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder =
-//                new AuthenticationManagerBuilder((ObjectPostProcessor<Object>) http.getSharedObject(org.springframework.beans.factory.BeanFactory.class));
-//        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
-//        return authenticationManagerBuilder.build();
-//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .requiresChannel(rcc-> rcc.anyRequest().requiresSecure())//only https is accepted
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/student-info/save").permitAll()
@@ -52,8 +35,9 @@ public class AppSecurity {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-//        return configuration.getAuthenticationManager();
-//    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 }
